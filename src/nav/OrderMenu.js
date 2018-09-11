29a9233e680958fd64e9a61ab9041adf293e945c
fadzar.wahyu <fadzar.wahyu@gmail.com>
2018-09-11 17:43:12 +0800

    add axios as fetch data api

--- src/nav/OrderMenu.js
+++ src/nav/OrderMenu.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react';
+import AvailableMenu from '../components/AvailableMenu';
+import ChosenMenu from '../components/ChosenMenu';
+import Axios from 'axios';
 
-export default () => {
-  return (
-    <div>
-      <h1>This Is Order Menu</h1>
-    </div>
-  )
+class OrderMenu extends Component {
+  state={
+    availableMenu:[
+      {id:0,nama:'Nasi Goreng',harga:8000},
+      {id:1,nama:'Nasi Pecel',harga:6000},
+      {id:2,nama:'Nasi Bongkot',harga:7000},
+    ],
+    chosenMenu:[
+    ]
+  }
+  componentDidMount(){
+    Axios.get('http://localhost:3000/menu')
+    .then(json => {
+      console.log(json);
+      this.setState({availableMenu:json.data})
+    })
+  }
+
+  selection=(id,type = 'select')=>{
+    let add='chosenMenu';
+    let rm='availableMenu';
+    if(type!=='select'){
+      add='availableMenu';
+      rm='chosenMenu';
+    }
+    let newrm = this.state[rm];
+    const newadd = this.state[add];
+    for(let x=0;x<newrm.length;x++){
+      if(newrm[x].id===id){
+        newadd.push(newrm[x]);
+        break;
+      }
+    }
+    newrm = newrm.filter(menu=>{return menu.id!==id});
+    this.setState({
+      [rm]:newrm,
+      [add]:newadd
+    })
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col m4 s12">
+          <h4 className="center">Menu</h4>
+          <AvailableMenu availableMenu={this.state.availableMenu} selection={this.selection}/>
+        </div>
+        <div className="col m8 s12">
+          <h4 className="center">Menu Terpilih</h4>
+          <ChosenMenu chosenMenu={this.state.chosenMenu} selection={this.selection}/>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default OrderMenu;
