49b10737e8cfdd95193aaaedf4fcfe74d7417e9e
f <wahyufadz@gmail.com>
2018-09-16 13:59:39 +0800

    add function to search available menu

--- src/nav/OrderMenu.js
+++ src/nav/OrderMenu.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import MenuView from '../components/MenuView';
+import SearchMenu from '../components/SearchMenu';
 
 class OrderMenu extends Component {
   state={
+    initMenu:[
+
+    ],
     availableMenu:[
-      {id:0,nama:'Nasi Goreng',harga:8000},
-      {id:1,nama:'Nasi Pecel',harga:6000},
-      {id:2,nama:'Nasi Bongkot',harga:7000},
     ],
     chosenMenu:[
     ]
@@ -16,9 +17,20 @@ class OrderMenu extends Component {
   componentDidMount(){
     Axios.get('http://localhost:3030/menu')
     .then(json => {
-      console.log(json);
-      this.setState({availableMenu:json.data})
+      this.setState({initMenu:json.data,availableMenu:json.data});
+      console.log(this.state)
     })
+  }
+
+  searchMenu = (menu) =>{
+    const initMenu=this.state.initMenu.filter(test=>{
+      return !(this.state.chosenMenu.includes(test));
+    });
+    const availableMenu=initMenu.filter(test=>{
+      return test.nama.indexOf(menu)!==-1?true:false;
+    })
+    this.setState({availableMenu:availableMenu});
+    
   }
 
   selection=(id,type = 'select')=>{
@@ -48,6 +60,7 @@ class OrderMenu extends Component {
       <div className="row">
         <div className="col m4 s12">
           <h4 className="center">Menu</h4>
+          <SearchMenu searchMenu={this.searchMenu}/>
           <MenuView menuView={this.state.availableMenu} selection={this.selection}/>
         </div>
         <div className="col m8 s12">
