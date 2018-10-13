c462a377179cd6409ef20ec52de5bc5b6e5d00fb
Unknown <wahyufadz@gmail.com>
2018-10-13 09:53:30 +0800

    fix css framework google icons and materialize css to offline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
+import {AutoInit} from'materialize-css'
+import 'material-design-icons/iconfont/material-icons.css'
+import './App.css'
+
 class App extends Component {
+  componentDidMount(){
+    AutoInit();
+  }
   render() {
     return (
       <div className="App container">
